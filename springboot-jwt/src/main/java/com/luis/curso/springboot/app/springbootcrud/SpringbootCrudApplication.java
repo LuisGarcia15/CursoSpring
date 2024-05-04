c7dba24d6dfd52be866b34d5161f43913cb76f30
Luis <luis200107@icloud.com>
2024-05-03 21:50:06 -0600

    Añadiendo configuración para correr nuestra aplicación de Spring en otro servidor tomcat. añadimos dependencias para crear el archivo .war y el nombre del archivo .war, además decargamos las herramientas del servidor tomcat ejecutamos el servidor junto a la aplicación

--- springboot-jwt/src/main/java/com/luis/curso/springboot/app/springbootcrud/SpringbootCrudApplication.java
+++ springboot-jwt/src/main/java/com/luis/curso/springboot/app/springbootcrud/SpringbootCrudApplication.java
@@ -2,9 +2,14 @@ package com.luis.curso.springboot.app.springbootcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootCrudApplication {
+public class SpringbootCrudApplication extends SpringBootServletInitializer{
+	/*SpringBootServletInitizalizer
+	 * Pemrite configurar un servidor externo al envevido por defecto al que incluye
+	 * springboot
+	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudApplication.class, args);
