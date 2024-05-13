1c88a1bd51b0e796ac6743daf8a7aa5bd2df7a7c
Luis <luis200107@icloud.com>
2024-05-13 09:10:38 -0600

    estudiando primeros bosquejos a los componentes en angular, familiarizando con la etiqueta igIf para un bloque de decisión

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+/*Un componente debe ser anotado con la notación component*/
 @Component({
+  /*El selector es aque nombre del componente al cual haremos
+  referencia para usarlo en la aplicación*/
   selector: 'app-root',
+  /* Indica que un componente no depende de modulos. Una aplicacion
+  de angulas no es modular*/
   standalone: true,
-  imports: [RouterOutlet],
+  /*Modulos necesarios para el funcionamiento*/
+  imports: [RouterOutlet, CommonModule],
+  /*Vista asociada al componente y estilos asociados al componente.
+  esa vista es única para este componente*/
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hola mundo angular 17';
+  enabled : boolean = false;
+
+  setEnabled(): void{
+    /*Similar al operador ternario en java*/
+    this.enabled = this.enabled? false: true;
+  }
 }
+
+/*Se tiene una hoja de estilos global y una página html global para toda
+la aplicación, siendo style e index respectivamente*/
